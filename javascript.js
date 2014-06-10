bfe3c40660134a47a8104fa126df587e0ab9de4a
roeku <donmezfurkan@gmail.com>
2014-06-10 16:51:39 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,147 +1,95 @@
-//********** 21:20 | getElementsById =/= getElementsByClassName
-
+//********** just the javascript used in the index.html
       
-function play() { 
-     //  var checkbox0 = document.getElementsById("checkbox0");
+ function play() {
+            window.setInterval(playWhenBoxesActive, 4080);
 
-         window.setInterval(playWhenBoxesActive, 4080);
-}   
-      
-function playWhenBoxesActive() {
+        }
 
-    var video = document.getElementById("video1");
-    for (var i = 0; i <= 7; i++) {
-    var boxesChecked = document.getElementById("checkbox" +i).checked;
-        if (boxesChecked == true) {
-            console.log(boxesChecked);
-            var speed = i*500+10;
-              var startLoop = window.setTimeout(
-                function(){
-                    video.play();
-                    console.log(video.currentTime);
-                    video.currentTime = 0;
-                    console.log(video.currentTime);
-                    console.log("checkbox" +i);
-                }, speed);
-            console.log(speed);
-           
-        } 
-    }
-}
+        function playWhenBoxesActive() {
+            for (var i = 0; i <= 7; i++) {
 
-//********* 19:11
 
-function play() { 
-     //  var checkbox0 = document.getElementsById("checkbox0");
+                var video0 = document.getElementById("video0");
+                var video1 = document.getElementById("video1");
+                var video2 = document.getElementById("video2");
+                var video3 = document.getElementById("video3");
+                var video4 = document.getElementById("video4");
+                var video5 = document.getElementById("video5");
 
-         window.setInterval(playWhenBoxesActive, 6000);
-}   
-      
-function playWhenBoxesActive() {
+                var boxesChecked = document.getElementById("checkbox" + i + 1);
+                var boxesChecked2 = document.getElementById("checkbox" + i + 2);
+                var boxesChecked3 = document.getElementById("checkbox" + i + 3);
+                var boxesChecked4 = document.getElementById("checkbox" + i + 4);
+                var boxesChecked5 = document.getElementById("checkbox" + i + 5);
+                var boxesChecked6 = document.getElementById("checkbox" + i + 6);
+                if (boxesChecked.checked == true) {
+                    var speed = i * 500 + 10;
+                    var startLoop = window.setTimeout(
+                        function () {
+                            video0.play();
+                            video0.currentTime = 0;
+                        }, speed);
+                } else {
+                    // console.log("no beat on 1");
+                }
 
-    var video = document.getElementById("video1");
-    for (var i = 0; i <= 7; i++) {
-    var boxesChecked = document.getElementById("checkbox" +i).checked;
-        if (boxesChecked == true) {
+                if (boxesChecked2.checked == true) {
+                    window.setTimeout(
+                        function () {
+                            video1.play();
+
+                            video1.currentTime = 0;
+                        }, speed);
+                } else {
+                    //console.log("no beat on 2");
+                }
+                if (boxesChecked3.checked == true) {
+                    window.setTimeout(
+                        function () {
+                            video2.play();
+                            video2.currentTime = 0;
+                        }, speed);
+                } else {
+                    //console.log("no beat on 3");
+                }
+                if (boxesChecked4.checked == true) {
+                    window.setTimeout(
+                        function () {
+                            video3.play();
+                            video3.currentTime = 0;
+                        }, speed);
+                } else {
+                    //    console.log("no beat on 4");
+                }
+                if (boxesChecked5.checked == true) {
+                    window.setTimeout(
+                        function () {
+                            video4.play();
+                            video4.currentTime = 0;
+                        }, speed);
+                } else {
+                    //  console.log("no beat on 5");
+                }
+                if (boxesChecked6.checked == true) {
+                    window.setTimeout(
+                        function () {
+                            video5.play();
+                            video5.currentTime = 0;
+                        }, speed);
+                } else {
+                    //console.log("no beat on 6");
+                }
+            }
+        }
+        function trailer () {
             
-            var speed = i*1000;
-              var startLoop = window.setTimeout(
-                function(){
-                    video.play();}, speed);
-            console.log("checkbox" +i);
-        } else {
-            video.pause();
-            clearInterval(startLoop);
-               }
-    }
-}
-
-
-
-//********** CHECKBOX
-
-function myFunction() {
-    var coffee = document.forms[0].coffee;
-    var txt = "";
-    var i;
-    for (i = 0; i < coffee.length; i++) {
-        if (coffee[i].checked) {
-            txt = txt + coffee[i].value + " ";
-        }
-    }
-    document.getElementById("order").value = "You ordered a coffee with: " + txt;
-}
-
-
-//********** SPEED
-
-var myVar=setInterval(function(){myTimer()},1000);
-
-function myTimer() {
-    var d = new Date();
-    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-}
-
-//********** RESET VIDEO
-
-myVid.play();
-myVid.addEventListener("canplay",function() { myVid.currentTime = 0;});
-
-//********** 
-
-function checkCheckboxes() {
-    var checkbox = document.forms[0].checkbox;
-    var i;
-    for(i = 0; i < checkbox.length; i++) {
-        if (checkbox[i].checked) {
+         var trailer = document.getElementById("myVideo");
+         var stop = 4;
+         trailer.currentTime = 0;
+            console.log(trailer.currentTime);
             
+            if (stop < trailer.currentTime) {
+                console.log(trailer.currentTime);
+                document.getElementById("t1").innerHTML = "<h1 class=\"center aligned ui header\">Interactive  <i class='music icon'></i> video</h1><h2 class=\"center aligned ui header\">make your own music anywhere, anytime with anything!</h2><h2 class=\"center aligned ui header\"><i class=\"arrow sign down icon large inverted\"></i></h2>";
         }
     }
-}
-
-//********** 
-
-function play() {
-       var video = document.getElementById("video1");
-       var checkbox = document.getElementById("checkbox");
-       if (video.paused) {
-          video.play();
-       } else {
-          video.pause();
-       }
-    }
-
-function loopCheckboxes() {
-    for (var i = 0; i < 7; i++) {
-        if (checkbox[i].checked) {
-            video.play();
-            setInterval(video.pause();, 1000);
-        }
-    }
-}
-
-function play() {
-       var video = document.getElementById("video1");
-       var checkbox = [document.getElementsByClassName("checkbox0"), 
-                       document.getElementsByClassName("checkbox1"),
-                       document.getElementsByClassName("checkbox2"),
-                       document.getElementsByClassName("checkbox3") ];
-   if(video.paused){    
-    for (var i = 0; i < 3; i++) {
-        if (checkbox[i].checked) {
-            video.play();
-
-        }
-    }
-}
-}
-
-      function checkCheckboxes() {
-    var checkbox = document.forms[0].checkbox;
-    var i;
-    for(i = 0; i < checkbox.length; i++) {
-        if (checkbox[i].checked) {
-            
-        }
-    }
-}
